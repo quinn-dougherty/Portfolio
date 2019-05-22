3fe240c1e8840ba790244d0f338c4274a1a8af4f
Quinn Dougherty <quinn.dougherty92@gmail.com>
2019-05-21 22:18:05 -0400

    edit readme

--- matcher/main.py
+++ matcher/main.py
@@ -1,62 +1,70 @@
 #!/usr/bin/env python
 
-from typing import List
-from matcher import *
 from argparse import ArgumentParser
+from itertools import product
+from typing import List
+from matcher import Person, Project, PREF_MAX, unassigned_num, assign_to_nth, mean_satisfaction, popular, unpopular
+from matcher import NUM_PROJECTS, NUM_PEOPLE, ALPHABET, PEOPLE, PROJECTS
 
 parser = ArgumentParser(description='Solve the match with random preferences')
-parser.add_argument('--popularity-threshold', type=int, default=4,
-                    help='how popular does a project have to be to be considered for duplication?')
+parser.add_argument('--popularity-threshold', type=int, default=3,
+                    help='how popular does a project have to be to be considered for duplication? Default: surplus popularity > 4')
 parser.add_argument('--unpopularity-threshold', type=int, default=4,
-                    help='how many unpopular projects to print?')
+                    help='how many unpopular projects to print? Default: 4')
 parser.add_argument('--poisson', type=bool, default=False,
-                    help="Use a poisson process to simulate unusually popular projects?")
+                    help="Use a poisson process to simulate unusually popular projects? Default: False")
+parser.add_argument('--reset', type=bool, default=True, help='reset assignments? Default: True')
 
 args = parser.parse_args()
+RESET = bool(args.reset)
 
-
-def solve(people: List[Person] = PEOPLE,
+def solve(people: List[Person],
+          projects: List[Project],
           iters: int = PREF_MAX,
           report: bool = True,
-          poisson: bool = False):
+          poisson_bool: bool = False):
     ''' solves. Prints stepwise report if bool=True.
     prints final report either way.  '''
-    if report:
-        for k in range(iters):
-            unassigned = unassigned_num(people)
-            print(f"{unassigned} are still unassigned..")
-            assign_to_nth(PEOPLE, k)
 
-    else:
-        for k in range(iters):
-            assign_to_nth(PEOPLE, k, poisson=poisson)
+    for k in range(iters): 
+        if report: 
+            print(f"{unassigned_num(people)} are still unassigned. ")
+        # solve
+        assign_to_nth(people, k, poisson_bool=bool(poisson_bool))
 
-    print(f"Match finished. {unassigned} are currently unmatched. ")
+    print(f"Match finished. {unassigned_num(people)} are left unmatched. ")
 
     popularity_report = ', '.join(str(x)
                                   for x
-                                  in popular(PROJECTS,
+                                  in popular(projects,
                                              threshold=args.popularity_threshold))
 
     unpopularity_report = ', '.join(str(x)
                                     for x
-                                    in unpopular(PROJECTS,
+                                    in unpopular(projects,
                                                  threshold=args.unpopularity_threshold))
 
     print()
-    print("In the following, the tuple (project, int) represents a project annotated by how many times someone was rejected from it. ")
-    print(f"The projects {popularity_report} are very popular. ")
+    
+    popularity_tuples = popular(projects, threshold=args.popularity_threshold)
+    if not popularity_report: 
+        print(f"No projects have more than {args.popularity_threshold} surplus popularity. ")
+    else: 
+        print("\n".join(f"Project {t[0]} has {t[1]} surplus popularity" for t in popularity_tuples))
     print()
 
-    print("In the following, the tuple (project, int) represents a project annotated by how many people are in it. ")
-    print(f"The projects {unpopularity_report} are rather unpopular")
+    unpopularity_tuples = unpopular(projects, threshold=args.unpopularity_threshold)
+    if not unpopularity_report: 
+        print(f"No projects have less than {args.unpopularity_threshold} people. ")
+    else: 
+        print("\n".join(f"Project {t[0]} as {t[1]} people assigned to it. " for t in unpopularity_tuples))
 
     print()
-    tot_sat = mean_satisfaction(PEOPLE)
+    tot_sat = mean_satisfaction(people)
     print(f"The mean satisfaction of this match is {tot_sat}. ")
     pass
 
 
 if __name__ == '__main__':
-    from matcher import *
-    solve(PEOPLE, poisson=args.poisson)
+
+    solve(PEOPLE, PROJECTS, poisson_bool=bool(args.poisson))
--- matcher/matcher.py
+++ matcher/matcher.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+
 ''' Matching algorithm, similar to Roth-Peranson's resident matching services but a slightly different usecase. '''
 from random import choice
-from typing import Optional, List, Tuple
+from typing import Optional, List, Tuple, Any
 from itertools import product
 from math import isclose
 from numpy import divide, clip  # type: ignore
@@ -15,34 +16,32 @@ PEOPLE_PER_PROJECT: int = NUM_PEOPLE // NUM_PROJECTS + 0
 ALPHABET: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 # two wrapper classes
-
-
 class Person(str):
     def __init__(self, name: str):
         ''' currently initializing to random preferences '''
         self.name: str = name
-
         # to simulate unusually popular projects
         self.preferences_poisson: List[Project] = [
-            PROJECTS[clip(poisson.rvs(mu=9), 0, NUM_PROJECTS - 1)] for _ in range(LEN_PREFS)]
-
+            PROJECTS[clip(poisson.rvs(mu=14), 0, NUM_PROJECTS - 1)]
+                          for _ in range(LEN_PREFS)]
         # regular randomness
         self.preferences: List[Project] = [
             choice(PROJECTS) for _ in range(LEN_PREFS)]
-
         # project person is assigned to.
         self.assigned: Optional[Project] = None
 
     def __str__(self):
         return self.name
 
-
 class Project(str):
     def __init__(self, proj_name: str,
                  max_team_size: int = PEOPLE_PER_PROJECT):
         self.proj_name: str = proj_name
+        # The most people the team can support.
         self.max_team_size: int = max_team_size
+        # The people on the team
         self.team: List[Person] = list()
+        # surplus popularity measure.
         self.popular: int = 0
 
     def __str__(self):
@@ -54,7 +53,9 @@ class Project(str):
             name.assigned = self
         else:
             self.popular += 1
-            return None  # print("CANT FIT ", name, " into this project!")
+            #print(self.popular)
+            pass  # print("CANT FIT ", name, " into this project!")
+
 
 
 # **** MORE GLOBAL VARS ****
@@ -64,7 +65,7 @@ PEOPLE: List[Person] = [Person(f"{x[0]}{x[1]}")
                         for x
                         in product(ALPHABET.lower(), ALPHABET.lower())][:NUM_PEOPLE]
 
-PREF_MAX: int = max([len(person.preferences) for person in PEOPLE])
+PREF_MAX: int = LEN_PREFS #max([len(person.preferences) for person in PEOPLE])
 
 MAX_TEAM: int = max([project.max_team_size for project in PROJECTS])
 
@@ -77,13 +78,13 @@ def get_unassigned(people: List[Person]) -> List[Person]:
 
 def assign_to_nth(people: List[Person],
                   n: int,
-                  poisson: bool = False):
+                  poisson_bool: bool = False):
     ''' Assign a person to their nth preference.
 
     poisson will generate random preferences with a poisson process, to simulate
     '''
     for pers in get_unassigned(people):
-        if poisson:
+        if poisson_bool:
             if not pers.assigned:
                 nth = pers.preferences_poisson[n]
                 nth.add_person(pers)
@@ -140,11 +141,11 @@ def unassigned_num(people: List[Person]) -> int:
 
 
 def popular(projects: List[Project],
-            threshold: int = 0) -> List[Tuple[Project, int]]:
+            threshold: int = 2) -> List[Tuple[Project, int]]:
     ''' sort projects by the popularity incrementer '''
-    return sorted([(project, project.popular) for project in projects if project.popular > 0],
+    return sorted([(project, project.popular) for project in projects if project.popular > threshold],
                   reverse=True,
-                  key=lambda project_tup: project_tup[0].popular)
+                  key=lambda project_tup: project_tup[1])
 
 
 def unpopular(projects: List[Project],
@@ -152,10 +153,10 @@ def unpopular(projects: List[Project],
     ''' show projects with the lowest amount of people '''
     return sorted([(project, len(project.team)) for project in projects],
                   reverse=False,
-                  key=lambda project_tup: len(project_tup[0].team))[:threshold]
+                  key=lambda project_tup: project_tup[1])[:threshold]
 
 
 ###############################
 # * * * * UNIT TEST * * * * *
 ###############################
-assert isclose(mean_satisfaction(PEOPLE), 0)
+# assert isclose(mean_satisfaction(PEOPLE), 0)
